bc2f73dae7de4465ed66c14c4da304f252e75ac4
Robert-Rendell <rob_452@hotmail.co.uk>
2021-11-01 15:20:21 +0000

    Temporarily disable CORS

--- index.ts
+++ index.ts
@@ -16,7 +16,7 @@ const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 app.use(helmet());
-app.use(cors());
+// app.use(cors()); // Don't use CORS for now
 app.use(morgan('combined'));
 app.use(express.json());
 app.listen(PORT, () => {
