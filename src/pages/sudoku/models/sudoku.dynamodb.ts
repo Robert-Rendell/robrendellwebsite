cb83836667adc4c1e8e3cfd269f573850ff27228
Robert-Rendell <rob_452@hotmail.co.uk>
2021-11-16 16:37:39 +0000

    added interchangeable attributes for sudoku dynamodb table

--- src/pages/sudoku/models/sudoku.dynamodb.ts
+++ src/pages/sudoku/models/sudoku.dynamodb.ts
@@ -1,9 +1,24 @@
 import DynamoDBString from '../../../models/dynamo-db';
 
 interface SudokuDynamoDB {
+  /**
+   * 'puzzle' is interchangeable with 'problem'.
+   */
+  puzzle: DynamoDBString,
+  /**
+   * 'problem' is interchangeable with 'puzzle'.
+   */
   problem: DynamoDBString,
   solution: DynamoDBString,
   sudokuId: DynamoDBString,
+
+  /**
+   * 'dateGenerated' is interchangeable with 'generatedDate'
+   */
+  dateGenerated: DynamoDBString,
+  /**
+   * 'generatedDate' is interchangeable with 'dateGenerated'
+   */
   generatedDate: DynamoDBString,
 }
 
