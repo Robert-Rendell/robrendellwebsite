5e6d850301268e40231f29cb4f4a81fecabe78e6
Rob <rob_452@hotmail.co.uk>
2022-10-29 13:30:59 +0100

    Fixed a bug where S3ImagePrefix had invalid paths

--- src/enums/s3-image-prefix.enum.ts
+++ src/enums/s3-image-prefix.enum.ts
@@ -1,8 +1,8 @@
 export enum S3ImagePrefix {
   Original = 'original',
   Thumbnail = '125x100',
-  WildFlowers = 'photos-ive-taken/nature/wild-flowers',
-  Lichen = 'photos-ive-taken/nature/lichen',
-  Arachnids = 'photos-ive-taken/nature/arachnids',
-  Insects = 'photos-ive-taken/nature/insects'
+  WildFlowers = 'nature/wild-flowers',
+  Lichen = 'nature/lichen',
+  Arachnids = 'nature/arachnids',
+  Insects = 'nature/insects'
 }
