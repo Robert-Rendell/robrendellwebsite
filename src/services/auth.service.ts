4a9c92381bcdc9ad9159ddb9717bbe47a2a301ed
Rob <rob_452@hotmail.co.uk>
2023-01-28 21:19:06 +0000

    Standalone - add auth service to support tokens

---
+++ src/services/auth.service.ts
@@ -0,0 +1,12 @@
+import { Request } from "express";
+import { ConfigService } from "./config.service";
+import { IPAddressService } from "./ip-address.service";
+
+export class AuthService {
+  public static hasAccess(req: Request): boolean {
+    return (
+      req.headers.authorization === ConfigService.KnockKnockSecurityKey ||
+      IPAddressService.isOneOfMyIpAddresses(req)
+    );
+  }
+}
