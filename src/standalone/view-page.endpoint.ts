c4d8dba7872e04a12848af972c09071754aa6b9b
Rob <rjprendell@gmail.com>
2023-02-17 11:24:05 +0000

    Standalone - remove page views array from save page view response for GDPR

--- src/standalone/view-page.endpoint.ts
+++ src/standalone/view-page.endpoint.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
-import { PageViewDto, PageViewerDocument } from "robrendellwebsite-common";
+import { PageViewRequest, ViewPageResponse } from "robrendellwebsite-common";
 import { ViewPageFunc } from "./view-page.function";
 
 export const SavePageView = async (
-  req: Request<PageViewDto>,
+  req: Request<PageViewRequest>,
   res: Response
 ) => {
   try {
-    const pageViewDocument: PageViewerDocument = await ViewPageFunc(req);
-    res.status(200).send(pageViewDocument);
+    const response: ViewPageResponse = await ViewPageFunc(req);
+    res.status(200).send(response);
   } catch (e) {
     console.log(e);
     res.status(500).send((e as Error).message);
