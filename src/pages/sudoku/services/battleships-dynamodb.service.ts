a6a7aedfe026e08b7bb93a98203aab70b77f94d5
Rob <rjprendell@gmail.com>
2023-03-02 07:28:33 +0000

    Battleships - add dynamodb tables and initial models

---
+++ src/pages/sudoku/services/battleships-dynamodb.service.ts
@@ -0,0 +1,68 @@
+import AWS from "aws-sdk";
+import {
+  BattleshipsGame,
+  BattleshipsStartConfiguration,
+  BattleshipsUser,
+} from "robrendellwebsite-common";
+import { ConfigService } from "../../../services/config.service";
+import DynamoDBService from "../../../services/dynamo-db.service";
+
+export default class BattleshipsDynamoDbService extends DynamoDBService {
+  private static PartitionKey = "gameId";
+  private static UserPartitionKey = "username";
+
+  public static async saveStartConfiguration(
+    startConfiguration: BattleshipsStartConfiguration
+  ): Promise<void> {
+    const marshalled = AWS.DynamoDB.Converter.marshall(startConfiguration);
+    super.save(ConfigService.SudokuSubmissionsDynamoDbTable, marshalled);
+  }
+
+  public static async saveGame(game: BattleshipsGame): Promise<void> {
+    const marshalled = AWS.DynamoDB.Converter.marshall(game);
+    super.save(ConfigService.SudokuSubmissionsDynamoDbTable, marshalled);
+  }
+
+  public static async saveUser(user: BattleshipsUser): Promise<void> {
+    const marshalled = AWS.DynamoDB.Converter.marshall(user);
+    super.save(ConfigService.SudokuSubmissionsDynamoDbTable, marshalled);
+  }
+
+  public static async loadStartConfiguration(
+    key: string
+  ): Promise<BattleshipsGame | undefined> {
+    const attributeMap = await super.load(
+      ConfigService.SudokuSubmissionsDynamoDbTable,
+      BattleshipsDynamoDbService.PartitionKey,
+      key
+    );
+    if (!attributeMap) return undefined;
+    return AWS.DynamoDB.Converter.unmarshall(attributeMap) as BattleshipsGame;
+  }
+
+  public static async loadGame(
+    key: string
+  ): Promise<BattleshipsStartConfiguration | undefined> {
+    const attributeMap = await super.load(
+      ConfigService.SudokuSubmissionsDynamoDbTable,
+      BattleshipsDynamoDbService.PartitionKey,
+      key
+    );
+    if (!attributeMap) return undefined;
+    return AWS.DynamoDB.Converter.unmarshall(
+      attributeMap
+    ) as BattleshipsStartConfiguration;
+  }
+
+  public static async loadUser(
+    key: string
+  ): Promise<BattleshipsUser | undefined> {
+    const attributeMap = await super.load(
+      ConfigService.SudokuSubmissionsDynamoDbTable,
+      BattleshipsDynamoDbService.UserPartitionKey,
+      key
+    );
+    if (!attributeMap) return undefined;
+    return AWS.DynamoDB.Converter.unmarshall(attributeMap) as BattleshipsUser;
+  }
+}
