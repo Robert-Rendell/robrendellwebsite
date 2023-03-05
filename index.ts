a81552793dec27fcedd0af2eb3b420afcdcaf680
Rob <rjprendell@gmail.com>
2023-03-05 18:23:51 +0000

    Battleships: relax rate limiting for regular game state checking, might move this to s3

--- index.ts
+++ index.ts
@@ -39,8 +39,8 @@ app.use(express.json());
 // eg. (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc)
 app.enable("trust proxy");
 const limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 15 minutes
+  max: 10000, // limit each IP to 100 requests per windowMs
 });
 
 // apply to all requests
