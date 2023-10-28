e1ada70533814ba3a32f3b1c05bbb7c92713a9cb
rob_452@hotmail.co.uk <robertrendell@hotmail.com>
2023-10-28 06:37:28 +0100

    Adjust webhooks endpoint

--- src/standalone/webhooks.endpoint.ts
+++ src/standalone/webhooks.endpoint.ts
@@ -4,12 +4,16 @@ import S3BucketService from "../services/s3-bucket.service";
 
 export const WebhooksEndpoint = async (req: Request, res: Response) => {
   try {
-    await S3BucketService.upload(
-      ConfigService.PublicBucket,
-      req.params.category,
-      req.body
-    );
-    res.status(200).send();
+    if (req.params.category) {
+      await S3BucketService.upload(
+        ConfigService.PublicBucket,
+        "webhooks/" + req.params.category,
+        req.body
+      );
+      res.status(200).send();
+    } else {
+      res.status(400).send("Error: 'category' not specified in query params");
+    }
   } catch (e) {
     res.status(500).send((e as Error).message);
   }
