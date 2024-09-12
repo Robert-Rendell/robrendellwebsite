c8e2cc2897d72faede0cf86fac93638a669ed437
Rob R <rob_452@hotmail.co.uk>
2024-09-12 13:06:32 +0100

    support for new host formats request headers differently

--- src/services/ip-address.service.ts
+++ src/services/ip-address.service.ts
@@ -6,19 +6,16 @@ import { ConfigService } from "./config.service";
 import axios, { AxiosResponse } from "axios";
 
 export class IPAddressService {
-  /**
-   * https://stackoverflow.com/questions/10849687/express-js-how-to-get-remote-client-address
-   * If you are running behind a proxy like NGiNX or what have you, only then you should
-   * check for 'x-forwarded-for':
-   * var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-   * If the proxy isn't 'yours', I wouldn't trust the 'x-forwarded-for' header,
-   * because it can be spoofed.
-   */
   public static getIPAddress(req: Request<unknown>): string | string[] {
-    const ip =
-      req.headers["x-forwarded-for"] ||
-      req.socket.remoteAddress ||
-      "unknown IP";
+    const forwarded = req.headers["x-forwarded-for"];
+    if (Array.isArray(forwarded)) {
+      return forwarded[0];
+    }
+    const split = forwarded?.split(",");
+    if (split && split.length > 1) {
+      return split[0];
+    }
+    const ip = forwarded || req.socket.remoteAddress || "unknown IP";
     return ip;
   }
 
