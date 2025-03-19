4d2d22800c1713eed4e4b789ba6ba0ad2212ed03
Rob R <rob_452@hotmail.co.uk>
2025-03-19 16:29:31 +0800

    Widen access to dog breeds endpoint

--- src/standalone/robs-free-dog-walks/get-dog-breeds.ts
+++ src/standalone/robs-free-dog-walks/get-dog-breeds.ts
@@ -9,34 +9,30 @@ import { DogBreedsJson } from "./types/dog-breeds";
 
 export const GetDogBreedsEndpoint = async (req: Request, res: Response) => {
   try {
-    if (AuthService.hasAccess(req)) {
-      const payload: any = req.body;
-      if (!payload.date) {
-        return res.status(400).send(<ErrorResponse>{
-          errorMessage: "'date' not given in request body",
-        });
-      }
-      if (!payload.event) {
-        return res.status(400).send(<ErrorResponse>{
-          errorMessage: "'event' not given in request body",
-        });
-      }
-      if (!payload.school) {
-        return res.status(400).send(<ErrorResponse>{
-          errorMessage: "'school' not given in request body",
-        });
-      }
-
-      const s3Json = await S3BucketService.download(
-        ConfigService.PublicBucket,
-        ROBS_FREE_DOG_WALKS_FOLDER_KEY + DOG_BREEDS_JSON
-      );
-      let dogBreedsJson: DogBreedsJson = JSON.parse(s3Json?.toString() || "");
-
-      res.status(200).send(JSON.stringify(dogBreedsJson));
-    } else {
-      res.status(403).send(IPAddressService.blockedIpMessage);
+    const payload: any = req.body;
+    if (!payload.date) {
+      return res.status(400).send(<ErrorResponse>{
+        errorMessage: "'date' not given in request body",
+      });
     }
+    if (!payload.event) {
+      return res.status(400).send(<ErrorResponse>{
+        errorMessage: "'event' not given in request body",
+      });
+    }
+    if (!payload.school) {
+      return res.status(400).send(<ErrorResponse>{
+        errorMessage: "'school' not given in request body",
+      });
+    }
+
+    const s3Json = await S3BucketService.download(
+      ConfigService.PublicBucket,
+      ROBS_FREE_DOG_WALKS_FOLDER_KEY + DOG_BREEDS_JSON
+    );
+    let dogBreedsJson: DogBreedsJson = JSON.parse(s3Json?.toString() || "");
+
+    res.status(200).send(JSON.stringify(dogBreedsJson));
   } catch (e) {
     res.status(500).send((e as Error).message);
   }
