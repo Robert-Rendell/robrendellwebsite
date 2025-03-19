5fd5e1cbda8b4660bd826b0860a09d302ebe59a1
Rob R <rob_452@hotmail.co.uk>
2025-03-19 16:30:30 +0800

    Remove unused imports

--- src/standalone/robs-free-dog-walks/get-dog-breeds.ts
+++ src/standalone/robs-free-dog-walks/get-dog-breeds.ts
@@ -1,9 +1,6 @@
 import { Request, Response } from "express";
-import { AuthService } from "../../services/auth.service";
-import { ErrorResponse } from "robrendellwebsite-common";
 import S3BucketService from "../../services/s3-bucket.service";
 import { ConfigService } from "../../services/config.service";
-import { IPAddressService } from "../../services/ip-address.service";
 import { DOG_BREEDS_JSON, ROBS_FREE_DOG_WALKS_FOLDER_KEY } from "./const";
 import { DogBreedsJson } from "./types/dog-breeds";
 
